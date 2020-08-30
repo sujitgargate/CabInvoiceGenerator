c3f699f1d6e092b1a2880689e9019525a7d827f9
sujit gargate <sujitgargate5893@gmail.com>
2020-08-30 13:55:18 +0530

    Modified Constant Variable Names In Method

--- src/main/java/CabFareInvoice.java
+++ src/main/java/CabFareInvoice.java
@@ -2,8 +2,8 @@ public class CabFareInvoice {
 
 
    //Generates Fare For Cab Journey
-   public double getTotalFareForJourney(double distance, double costPerKm, double minutesSpent, double farePerMinute) {
-      double totalFare = distance * costPerKm + minutesSpent * farePerMinute;
+   public double getTotalFareForJourney(double distance, double COST_PER_KM, double minutesSpent, double FARE_PER_MINUTE) {
+      double totalFare = distance * COST_PER_KM + minutesSpent * FARE_PER_MINUTE;
       return totalFare;
    }
 }
