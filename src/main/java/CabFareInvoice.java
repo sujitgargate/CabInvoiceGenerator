a76f794694966426a35d4f9a849f93a80f163f63
sujit gargate <sujitgargate5893@gmail.com>
2020-08-30 13:49:32 +0530

    Modified Tests Becasue DRY Was Getting Violated

--- src/main/java/CabFareInvoice.java
+++ src/main/java/CabFareInvoice.java
@@ -1,6 +1,7 @@
 public class CabFareInvoice {
 
 
+   //Generates Fare For Cab Journey
    public double getTotalFareForJourney(double distance, double costPerKm, double minutesSpent, double farePerMinute) {
       double totalFare = distance * costPerKm + minutesSpent * farePerMinute;
       return totalFare;
--- src/test/java/CabFareTest.java
+++ src/test/java/CabFareTest.java
@@ -1,20 +1,28 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CabFareTest {
 
+   CabFareInvoice cabFareInvoice;
+
+   @Before
+   public void setUp() throws Exception {
+      cabFareInvoice = new CabFareInvoice();
+   }
+
+   //Tests Generated Fare And Expected Fare
    @Test
    public void givenDistanceAndTimeForJourney_ShouldReturnTotalFare() {
-      CabFareInvoice cabFareInvoice = new CabFareInvoice();
       double totalFareForJourney = cabFareInvoice.getTotalFareForJourney(2.0, 10, 5, 1);
       Assert.assertEquals(25, totalFareForJourney, 0.0);
    }
 
+   //Negative Tests For Generated Fare And Expected Fare
    @Test
    public void givenDistanceAndTimeForJourney_ShouldReturnFalseForNegativeTest() {
-      CabFareInvoice cabFareInvoice = new CabFareInvoice();
       double totalFareForJourney = cabFareInvoice.getTotalFareForJourney(2.0, 10, 5, 1);
-      Assert.assertNotEquals(totalFareForJourney, 25);
+      Assert.assertNotEquals(totalFareForJourney, 75);
    }
 }
 
