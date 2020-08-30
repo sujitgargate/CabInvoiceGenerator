8332c5ab69c58f06a46429868b284ac68da8e692
sujit gargate <sujitgargate5893@gmail.com>
2020-08-30 20:51:44 +0530

    Added Code To Calculate Muiltiple Rides

--- src/main/java/CabFareInvoice.java
+++ src/main/java/CabFareInvoice.java
@@ -1,12 +1,29 @@
+
 public class CabFareInvoice {
 
+   //CabFareInvoice cabFareInvoice = new CabFareInvoice();
+
+   public double calculateTotalFare(double distance, double COST_PER_KM, double minutesSpent, double FARE_PER_MINUTE) {
+      double totalFare = distance * COST_PER_KM + minutesSpent * FARE_PER_MINUTE;
+      return totalFare;
+   }
 
    //Generates Fare For Cab Journey
    public double getTotalFareForJourney(double distance, double COST_PER_KM, double minutesSpent, double FARE_PER_MINUTE) {
-      double totalFare = distance * COST_PER_KM + minutesSpent * FARE_PER_MINUTE;
+      double totalFare = this.calculateTotalFare(distance, COST_PER_KM, minutesSpent, FARE_PER_MINUTE);
+
       if(totalFare < 5) {
          return 5;
       }
       return totalFare;
+   }
+
+   //Genrates Total Fare For Muiltiple Journey
+   public double getTotalFareForMuiltipleJourney(Journey[] journey) {
+      double totalFareForMuiltipleJourney = 0;
+      for(Journey journeyloop:journey) {
+         totalFareForMuiltipleJourney = this.getTotalFareForJourney(journeyloop.distanceTravelled, journeyloop.COST_PER_KM, journeyloop.WaitingTime, journeyloop.FARE_PER_MIN);
+      }
+      return totalFareForMuiltipleJourney;
    }
 }
--- src/test/java/CabFareTest.java
+++ src/test/java/CabFareTest.java
@@ -6,6 +6,9 @@ public class CabFareTest {
 
    CabFareInvoice cabFareInvoice;
 
+   int COST_PER_KM = 10;
+   int FARE_PER_MIN = 1;
+
    @Before
    public void setUp() throws Exception {
       cabFareInvoice = new CabFareInvoice();
@@ -14,21 +17,30 @@ public class CabFareTest {
    //Tests Generated Fare And Expected Fare
    @Test
    public void givenDistanceAndTimeForJourney_ShouldReturnTotalFare() {
-      double totalFareForJourney = cabFareInvoice.getTotalFareForJourney(2.0, 10, 5, 1);
+      double totalFareForJourney = cabFareInvoice.getTotalFareForJourney(2.0, COST_PER_KM, 5, FARE_PER_MIN);
       Assert.assertEquals(25, totalFareForJourney, 0.1);
    }
 
    //Negative Tests For Generated Fare And Expected Fare
    @Test
-   public void givenDistanceAndTimeForJourney_ShouldReturnFalseForNegativeTest() {
-      double totalFareForJourney = cabFareInvoice.getTotalFareForJourney(2.0, 10, 5, 1);
+   public void givenDistanceAndTimeForJourney_ShouldPassTestForNegativeTest() {
+      double totalFareForJourney = cabFareInvoice.getTotalFareForJourney(2.0, COST_PER_KM, 5, FARE_PER_MIN);
       Assert.assertNotEquals(85, totalFareForJourney);
    }
 
+   //Test For Minimum Fare
    @Test
    public void givenDistanceAndTimeForJourney_ShouldReturnMinimumFare() {
-      double totalFareForJourney = cabFareInvoice.getTotalFareForJourney(0.2, 10, 0, 1);
+      double totalFareForJourney = cabFareInvoice.getTotalFareForJourney(0.2, COST_PER_KM, 0, FARE_PER_MIN);
       Assert.assertEquals(5, totalFareForJourney, 0.1);
+   }
+   @Test
+   public void givenDistanceAndTimeForMuiltipleJourney_ShouldReturnTotalFare() {
+      Journey[] muiltipleRides = {new Journey(2.0, COST_PER_KM, 5, FARE_PER_MIN),
+              new Journey(5.0, COST_PER_KM, 10, FARE_PER_MIN)
+      };
+      double totalFareForMuiltipleJourney = cabFareInvoice.getTotalFareForMuiltipleJourney(muiltipleRides);
+      Assert.assertEquals(60, totalFareForMuiltipleJourney, 0.0);
    }
 }
 
